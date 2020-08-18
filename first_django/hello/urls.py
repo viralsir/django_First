44bf6e18cb511235f0b73c4041dfe6e0063afa92
viralsir <viralsir2018@gmail.com>
2020-08-18 10:31:05 +0530

    render html pages into views

--- first_django/hello/urls.py
+++ first_django/hello/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns=[
     path("",views.index,name="index"),
+    path("<str:name>",views.greet,name="greet"),
     path("about",views.about,name="about"),
     path("avish",views.avish,name="avish"),
     path("dhruvil",views.dhruvil,name="dhruvil")
--- first_django/hello/views.py
+++ first_django/hello/views.py
@@ -2,13 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1> Hello world </h1>")
+    return render(request,"index.html")
 
 def avish(request):
-    return HttpResponse("<h1> Hello Avish </h1>")
+    return HttpResponse("<h1> Hello Avish </h1> ")
 
 def dhruvil(request):
     return HttpResponse("<h1> hello Dhruvil </h1>")
 
 def about(request):
     return HttpResponse("<h1> About Page </h1>")
+
+def greet(request,name):
+    return render(request,"greet.html",{
+        "name":name
+    })

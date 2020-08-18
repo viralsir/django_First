125f889cceb8d8644813d1b3e234dda50e936361
viralsir <viralsir2018@gmail.com>
2020-08-18 10:49:39 +0530

    newyear app added showing control structure in html

---
+++ first_django/newyear/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from datetime import datetime
+# Create your views here.
+def home(request):
+    now=datetime.now()
+
+    return render(request,"newyear/home.html",{
+        "newyear": now.month==1 and now.day==1
+    })
+
